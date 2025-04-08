6eaf21ed6c48f18bceaa907567307fe939d9699f
jcreddy096 <jcreddyvennapusa@gmail.com>
2025-04-08 12:29:19 +0530

    updated

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -1,10 +1,3 @@
-// import { ICartProduct } from './product';
-
-// export type ICartMap = Record<string, Record<string, ICartProduct>>;
-
-// export type IUserCartMap = Record<string, ICartProduct>;
-
-
 
 import { ICartProduct } from './product';
 
--- src/utils/cart.ts
+++ src/utils/cart.ts
@@ -1,42 +1,4 @@
 
-
-// import { ICartMap, IUserCartMap } from '@/types/cart';
-// import { IProduct } from '@/types/product';
-
-// export const getStorageCart = (): ICartMap => {
-//   if (typeof window === 'undefined') return {};
-//   return JSON.parse(localStorage.getItem('cart') || '{}');
-// };
-
-// export const setStorageCart = (cart: ICartMap) => {
-//   if (typeof window === 'undefined') return;
-//   localStorage.setItem('cart', JSON.stringify(cart));
-// };
-
-// export const getUserStorageCart = (email: string): IUserCartMap => {
-//   const cart = getStorageCart();
-//   const userCart = cart[email];
-//   return userCart && typeof userCart === 'object' && !Array.isArray(userCart) ? userCart : {};
-// };
-
-// export const setUserStorageCart = (email: string, userCart: IUserCartMap) => {
-//   const cart = getStorageCart();
-//   cart[email] = userCart;
-//   setStorageCart(cart);
-// };
-
-// export const getStorageProducts = (): IProduct[] => {
-//   if (typeof window === 'undefined') return [];
-//   const data = localStorage.getItem('products');
-//   return data ? JSON.parse(data) : [];
-// };
-
-
-
-
-
-
-
 import { ICartMap, IUserCartMap } from '@/types/cart';
 import { ICartProduct } from '@/types/product';
 
