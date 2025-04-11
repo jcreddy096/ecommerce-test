c0eddaa742e79c4605f8814b242ae7eb849232e5
jcreddy096 <jcreddyvennapusa@gmail.com>
2025-04-11 10:22:18 +0530

    completed

--- src/app/products/id/[id]/page.tsx
+++ src/app/products/id/[id]/page.tsx
@@ -3,30 +3,21 @@ import ProductCard from '@/app/components/ProductCard';
 import { Box } from '@mui/material';
 import AuthGuard from '@/app/auth/AuthGuard';
 
-interface PageProps {
+type Props = {
   params: {
     id: string;
   };
-}
+};
 
-
-const ProductDetailPage = async ({ params }: PageProps) => {
+const ProductDetailPage = async ({ params }: Props) => {
   const product = await fetchProductById(Number(params.id));
 
-  
   return (
-  
     <AuthGuard>
-  <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="80vh"
-    >
-  
-  <ProductCard product={product} showActions={true} fullWidth={true} userId={0} />
-  </Box>
-  </AuthGuard>
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
+        <ProductCard product={product} showActions={true} fullWidth={true} userId={0} />
+      </Box>
+    </AuthGuard>
   );
 };
 
