c05b43b49af1e4958e849812e2c125c9bff54a91
jcreddy096 <jcreddyvennapusa@gmail.com>
2025-04-11 10:48:57 +0530

    completed

--- src/app/products/id/[id]/page.tsx
+++ src/app/products/id/[id]/page.tsx
@@ -2,19 +2,12 @@ import { fetchProductById } from '@/app/utils/productApi';
 import ProductCard from '@/app/components/ProductCard';
 import { Box } from '@mui/material';
 import AuthGuard from '@/app/auth/AuthGuard';
+import { JSX } from 'react';
 
-
-interface ProductPageParams {
-  id: string;
-}
-
-interface ProductPageProps {
-  params: ProductPageParams;
-}
-
-export default async function ProductDetailPage({ params }: ProductPageProps) {
+export default async function ProductDetailPage({ params }: { params: { id: string } }): Promise<JSX.Element> {
   
   const id = Number(params.id);
+  
   const product = await fetchProductById(id);
 
   return (
